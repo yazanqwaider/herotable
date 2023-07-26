1fdc481b8954f1e096d04d9dc43321ea713b195f
yazanQwaider <yazeno15@gmail.com>
2023-07-26 22:19:53 +0300

    Add the initial code of hide columns feature

--- src/js/index.js
+++ src/js/index.js
@@ -25,7 +25,8 @@ $.fn.herotable = function() {
                 header_row.cols.push({
                     html: col.outerHTML,
                     value: $(col).text(),
-                    sort_by: ''
+                    sort_by: '',
+                    is_hidden: false
                 });
             });
     
@@ -39,6 +40,7 @@ $.fn.herotable = function() {
             const header_cols_length = $(header).find('th').length;
             header.find('th').each(function(header_col_index, header_col) {
                 initializeSearchInput(header_col_index, header_col);
+                initializeHideBtn(header_col_index, header_col);
                 initializeSortBtn(header_col_index, header_col);
                 initializeResizer(header_col_index, header_col, header_cols_length);
             });
@@ -61,7 +63,9 @@ $.fn.herotable = function() {
                         body_row.cols.push({
                             html: col.outerHTML,
                             value: $(col).text(),
-                            original: i == 0
+                            is_colspan: colspan > 1,
+                            original: i == 0,
+                            is_hidden: false
                         });
                     }
                 });
@@ -135,6 +139,36 @@ $.fn.herotable = function() {
                 });
         }
     
+        function initializeHideBtn(header_col_index, header_col) {
+            header_col = $(header_col);
+
+            const hide_sort_icon = '<svg clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" height="20" width="20" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m17.069 6.546 2.684-2.359c.143-.125.32-.187.497-.187.418 0 .75.34.75.75 0 .207-.086.414-.254.562l-16.5 14.501c-.142.126-.319.187-.496.187-.415 0-.75-.334-.75-.75 0-.207.086-.414.253-.562l2.438-2.143c-1.414-1.132-2.627-2.552-3.547-4.028-.096-.159-.144-.338-.144-.517s.049-.358.145-.517c2.111-3.39 5.775-6.483 9.853-6.483 1.815 0 3.536.593 5.071 1.546zm2.319 1.83c.966.943 1.803 2.014 2.474 3.117.092.156.138.332.138.507s-.046.351-.138.507c-2.068 3.403-5.721 6.493-9.864 6.493-1.297 0-2.553-.313-3.729-.849l1.247-1.096c.795.285 1.626.445 2.482.445 3.516 0 6.576-2.622 8.413-5.5-.595-.932-1.318-1.838-2.145-2.637zm-3.434 3.019c.03.197.046.399.046.605 0 2.208-1.792 4-4 4-.384 0-.756-.054-1.107-.156l1.58-1.389c.895-.171 1.621-.821 1.901-1.671zm-.058-3.818c-1.197-.67-2.512-1.077-3.898-1.077-3.465 0-6.533 2.632-8.404 5.5.853 1.308 1.955 2.567 3.231 3.549l1.728-1.519c-.351-.595-.553-1.289-.553-2.03 0-2.208 1.792-4 4-4 .925 0 1.777.315 2.455.843zm-2.6 2.285c-.378-.23-.822-.362-1.296-.362-1.38 0-2.5 1.12-2.5 2.5 0 .36.076.701.213 1.011z" fill-rule="nonzero"/></svg>';
+            header_col.append(`<span class="header-hide-icon">${hide_sort_icon}</span>`);
+    
+            // register the search input event
+            const header_hide_icon = header_col.find('.header-hide-icon');
+            header_hide_icon.on('click', function(e) {
+                // check if there and body column in the same index is not valid,
+                // such as it is colspan or it is not exist.
+                const invalid_col = body_rows_values.find((body_row) => {
+                    return header_col_index > body_row.cols.length - 1 || body_row.cols[header_col_index].is_colspan;
+                });
+
+                if(invalid_col) {
+                    alert('There are invalid columns, please remove the colspan or add the missing column to fix it.');
+                    return false;
+                }
+
+                header_col.hide();
+                header_rows_values[0].cols[header_col_index].is_hidden = true;
+
+                body_rows_values.forEach((body_row, body_row_index) => {
+                    table.find(`tbody tr:eq(${body_row_index}) td:eq(${header_col_index})`).hide();
+                    body_row.cols[header_col_index].is_hidden = true;
+                });
+            });
+        }
+
         function initializeSortBtn(header_col_index, header_col) {
             header_col = $(header_col);
     
