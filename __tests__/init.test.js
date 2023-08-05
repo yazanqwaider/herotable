7db01437b160a82ca33e03481aa5764e6d44afd7
yazanQwaider <yazeno15@gmail.com>
2023-08-05 22:23:19 +0300

    Add the sorting test

--- __tests__/init.test.js
+++ __tests__/init.test.js
@@ -3,15 +3,20 @@ beforeAll(() => {
     '<table>'+
         '<thead>'+
             '<tr>'+
-                '<th>1</th>'+
-                '<th>2</th>'+
+                '<th>header col 1</th>'+
+                '<th>header col 2</th>'+
             '</tr>'+
         '</thead>'+
 
         '<tbody>'+
             '<tr>'+
-                '<td>1</td>'+
-                '<td>2</td>'+
+                '<td>body col 1.1</td>'+
+                '<td>body col 1.2/td>'+
+            '</tr>'+
+
+            '<tr>'+
+                '<td>body col 2.1</td>'+
+                '<td>body col 2.2</td>'+
             '</tr>'+
         '</tbody>'+
     '</table>';
@@ -28,4 +33,10 @@ test('Don\'t initialize herotable more thant one time on table', function() {
     let instance_2 = $('table').herotable();
 
     expect(instance[0].herotable).toBe(instance_2[0].herotable);
+});
+
+
+test('Check the body rows count', function() {
+    let instance = $('table').herotable();
+    expect(instance[0].herotable.body_rows_values).toHaveLength(2);
 });
---
+++ __tests__/sort.test.js
@@ -0,0 +1,44 @@
+beforeAll(() => {
+    document.body.innerHTML = 
+    '<table>'+
+        '<thead>'+
+            '<tr>'+
+                '<th>header col 1</th>'+
+                '<th>header col 2</th>'+
+            '</tr>'+
+        '</thead>'+
+
+        '<tbody>'+
+            '<tr>'+
+                '<td>body col 1.1</td>'+
+                '<td>body col 1.2/td>'+
+            '</tr>'+
+
+            '<tr>'+
+                '<td>body col 2.1</td>'+
+                '<td>body col 2.2</td>'+
+            '</tr>'+
+        '</tbody>'+
+    '</table>';
+});
+
+
+test('Sort the first column ascendingly', function() {
+    let instance = $('table').herotable();
+    instance.find('thead tr th .header-sort-icon').first().trigger('click');
+    const first_body_col = instance.find('tbody tr td').first().text();
+    expect(first_body_col).toBe('body col 1.1');
+});
+
+test('Sort the first column descendingly', function() {
+    let instance = $('table').herotable();
+    const first_header_row_col = instance.find('thead tr th .header-sort-icon').first();
+
+    first_header_row_col.trigger('click');
+    setTimeout(() => {
+        first_header_row_col.trigger('click');
+    }, 100);
+
+    const first_body_row_col = instance.find('tbody tr td').first();
+    expect(first_body_row_col.text()).toBe('body col 2.1');
+});
